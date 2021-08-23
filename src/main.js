265b76994fefec3b9c25afc30547693909d5a013
best-lucky <bestlucky1030@gmail.com>
2021-08-24 00:01:20 +0300

    add some packages

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+import VueSweetalert2 from "vue-sweetalert2";
+
+Vue.use(VueSweetalert2);
+Vue.use(Buefy);
+
+// Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+  // router,
+  // store,
+  render: (h) => h(App),
+}).$mount("#app");
